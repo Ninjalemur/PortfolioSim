ba19a50f1549aff24534c3a284449df158119cff
Ninjalemur <ninjalemur33@gmail.com>
2021-08-10 14:00:46 +0800

    implemented test for _get_portfolio_value

--- tests/test_simulation_init.py
+++ tests/test_simulation_init.py
@@ -43,4 +43,46 @@ def test_simulation_portfolio_cash_buffer_initialisation():
 
     assert x.get_portfolio() == expected_portfolio
     assert x.get_cash_buffer() == expected_cash_buffer
+
+def test_simulation_get_portfolio_value():
+    """
+    ensure that simulation retrieves portfolio values correctly
+    via _get_portfolio_value
+    """
+
+    simulation_config = {
+        "starting_portfolio_value" : 1202,
+        "max_withdrawal_rate" : 0.99,
+        "income_schedule" : pd.DataFrame(data={
+            'year':pd.Series([1,2,3], dtype='int'),
+            'desired_income':pd.Series([100,102,104], dtype='float'),
+            'min_income':pd.Series([50,51,52], dtype='float')
+            }),
+        "historical_data_subset": pd.DataFrame(data={
+            'year':pd.Series([1,2,3], dtype='int'),
+            'month':pd.Series([1,2,3], dtype='float'),
+            'gold':pd.Series([5,10,20], dtype='float'),
+            'stocks':pd.Series([10,20,40], dtype='float'),
+            'bonds':pd.Series([50,100,200], dtype='float')
+            }),
+        "portfolio_allocation" : {
+            'stocks' : 1,
+            'gold' : 1,
+            'bonds' : 1,
+            'cash' : 1
+            },
+        "cash_buffer_years" : 2
+        }
+    
+    x = ps.Simulation(**simulation_config)
+    
+    expected_portfolio = {
+        'stocks' : 25.0,
+        'gold' : 50.0,
+        'bonds' : 5.0,
+        'cash' : 250.0
+        }
+    expected_portfolio_value = 1000
+
+    assert x._get_portfolio_value() == expected_portfolio_value
         
